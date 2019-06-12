f9930098279a50167133e316373a3261aa308b3b
iqbalreact <iqbalmobiledev@gmail.com>
2019-06-12 15:20:06 +0700

    add boostrap and create form submit

--- app/controllers/mahasiswas_controller.rb
+++ app/controllers/mahasiswas_controller.rb
@@ -4,9 +4,21 @@ class MahasiswasController < ApplicationController
   end
 
   def show
+    @mahasiswa = Mahasiswa.find(params[:id])
   end
 
   def new
+    @mahasiswa = Mahasiswa.new
+  end
+  
+  def create
+    mahasiswa = Mahasiswa.create(mahasiswa_params)
+    redirect_to mahasiswa_path 
+  end
+
+  private
+  def mahasiswa_params
+    params.require(:mahasiswa).permit(:nama, :jurusan)  
   end
 
   def edit
