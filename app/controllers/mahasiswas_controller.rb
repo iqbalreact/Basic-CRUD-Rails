82f86aae60a3bd55599ce0671f5f0ca805ce353b
iqbalreact <iqbalmobiledev@gmail.com>
2019-06-12 15:44:18 +0700

    add create done

--- app/controllers/mahasiswas_controller.rb
+++ app/controllers/mahasiswas_controller.rb
@@ -1,19 +1,33 @@
 class MahasiswasController < ApplicationController
+
+  # Menampilkan seluruh data mahasiswa
   def index
     @mahasiswas = Mahasiswa.all
   end
 
+  # Menampilkan data mahasiswa yang dipilih
   def show
     @mahasiswa = Mahasiswa.find(params[:id])
   end
-
+  
+  # menampilkan form untuk menambahkan mahasisa
   def new
     @mahasiswa = Mahasiswa.new
   end
   
+  # menyimpan data mahasiswa baru menggunakan parameter
   def create
-    mahasiswa = Mahasiswa.create(mahasiswa_params)
-    redirect_to mahasiswa_path 
+    @mahasiswa = Mahasiswa.new(mahasiswa_params)
+    if @mahasiswa.save
+      redirect_to mahasiswas_path
+    else
+      render 'new'
+      # kembali kehalaman form tambah mahasiswa/
+    end
+  
+  end
+
+  def edit
   end
 
   private
@@ -21,6 +35,4 @@ class MahasiswasController < ApplicationController
     params.require(:mahasiswa).permit(:nama, :jurusan)  
   end
 
-  def edit
-  end
 end
